304e484350dd3748b7b85b4d53f5bb8aa72eec3c
olivia-0101 <22328406@bucks.ac.uk>
2026-03-29 23:49:20 +0100

    Outlining the Classes

--- main/main.cpp
+++ main/main.cpp
@@ -5,6 +5,43 @@
 using namespace std; // efficiency, removes the need to type in std:: each time
 
 
+// creating the Book class
+class Book {
+
+};
+
+
+// creating the User class
+class User {
+
+};
+
+
+// creating the LibraryMember class
+class LibraryMember {
+
+};
+
+
+// creating the Librarian class
+class Librarian {
+
+};
+
+
+// creating the Administrator class
+class Administrator {
+
+};
+
+
+// creating the LibrarySystem class
+class LibrarySystem {
+
+};
+
+
+
 int main() { // start of the program
 	return 0;
 }
