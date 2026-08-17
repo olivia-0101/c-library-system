f6ed106a2f173fcae53d19aebfa25c2059f38297
olivia-0101 <22328406@bucks.ac.uk>
2026-08-17 20:53:15 +0100

    Adding more code and adding comments

--- main/Book.cpp
+++ main/Book.cpp
@@ -1,26 +1,46 @@
-#include "Book.h"
-#include <iostream>
+#include "Book.h" // includes Book header information
+#include <iostream> // allows for cout
 
-Book::Book(int id, string t, string a)
+Book::Book(int id, string t, string a) // template for creating a Book object
 {
-	bookId = id;
+	bookId = id; // attributes
 	title = t;
 	author = a;
-	status = BookStatus::Available;
+	status = BookStatus::Available; // each book starts as Available as per pseudocode
 	dueDate = "";
 }
 
-void Book::borrow()
+void Book::borrow() // borrowing method
 {
-	status = BookStatus::Borrowed;
+	status = BookStatus::Borrowed; // sets the book's status to 'Borrowed'
 }
 
-void Book::returnBook()
+void Book::returnBook() // returning method
 {
-	status = return status;
+	status = return status; // sets the book's status back to 'Available'
 }
 
-void Book::displayBookInfo()
+void Book::getStatus() // showing the book's status method
 {
+	return status; // simply displays it
+}
 
+void Book::displayBookInfo() // method for displaying a book's info
+{
+	cout << "ID: " << bookId << endl; // attributes will be displayed as per pseudocode
+	cout << "Title: " << title << endl;
+	cout << "Author: " << title << endl;
+
+	if (status == BookStatus::Available) // if a book is 'Available'
+	{
+		cout << "Status: Available" << endl;
+	}
+	else if (status == BookStatus::Reserved) // if a book is 'Reserved'
+	{
+		cout << "Status: Reserved" << endl;
+	}
+	else if (status == BookStatus::Borrowed) // if a book is 'Borrowed'
+	{
+		cout << "Status: Borrowed" << endl;
+	}
 }
