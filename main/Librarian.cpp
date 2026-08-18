00aa5cecc1d8f8cc46bfb5b07542a529daa83b02
olivia-0101 <22328406@bucks.ac.uk>
2026-08-18 16:18:35 +0100

    Starting Librarian methods, but need to do LibrarySystem first

---
+++ main/Librarian.cpp
@@ -0,0 +1,9 @@
+#include "Librarian.h" // includes LibraryMember header information
+#include <iostream> // allows for cout
+
+Librarian::Librarian(int id, string n, string e, string p)
+	: LibraryUser(id, n, e, p) // calling the parent class constructor to make the child object
+{
+}
+
+// cannot do methods yet because I need to do the Librarian class
