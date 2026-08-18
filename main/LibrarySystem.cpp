7a13ddeaa91a101e2bdeba288a11ac08c78ab271
olivia-0101 <22328406@bucks.ac.uk>
2026-08-18 16:51:24 +0100

    Library System methods continued

--- main/LibrarySystem.cpp
+++ main/LibrarySystem.cpp
@@ -34,3 +34,36 @@ vector<Book*> LibrarySystem::searchByAuthor(string title) // search by author me
 	}
 	return results; // results are then shown
 }
+
+
+bool LibrarySystem::checkAvailability(Book* book) // checking book availability method
+{
+	return book->getStatus() == BookStatus::Available; // if the book is available will return true, if not will return false
+}
+
+
+void LibrarySystem::sendAlert() // sending alerts
+{
+	cout << "Library system alert." << endl; // *****haven't figured out how I want the alerts to appear on the system yet, will return to this!
+}
+
+
+void LibrarySystem::handleReservations() // handling reservations
+{
+	cout << "Library reservations handled." << endl; // *****haven't figured out how I want the alerts to appear on the system yet, will return to this!
+}
+
+
+Book* LibrarySystem::findBook(string title) // finding a particular book method
+{
+	for (Book* book : books) // goes through all books in a for loop
+	{
+		if (book->getTitle() == title) // if the specific pointer book has the title we need...
+		{
+			return book; // gives us the first book
+		}
+	}
+	return nullptr; // otherwise returns nothing
+}
+
+
