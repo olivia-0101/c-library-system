28f92fde7d5e4a750995dbe9ba3c959d0d25c418
olivia-0101 <22328406@bucks.ac.uk>
2026-08-21 16:54:20 +0100

    Including all these classes in main

--- main/main.cpp
+++ main/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream> // allows input and output
 #include <vector> // for use of book vectors that will later have pointers inside of them!
 #include <string> // for all of the different string attributes
+#include "Book.h" // including all the classes
+#include "LibraryUser.h"
+#include "LibraryMember.h"
+#include "Librarian.h"
+#include "LibrarySystem.h"
 
 using namespace std; // efficiency, removes the need to type in std:: each time
 
